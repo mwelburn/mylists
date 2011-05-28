60430e1af5589cd8377cae750484d773db9a3e26
br0nc080 <mwelbu2@gmail.com>
2011-05-28 15:01:18 -0500

    commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,27 +12,36 @@ class HomeController < ApplicationController
 
   def foursquare
     client = Foursquare2::Client.new(:client_id => '4GI510HOP4UN1RT4F015WKIDH3JHQK3M1KZZV2AQHLMHGC3N', :client_secret => 'JX1YCRAVY4R0P4WUQSCCNWMMPOFGELBQAU2RYU13W4TE4FIK')
+    #client = Foursquare2::Client.new(:oauth_token => 'OV4XQWEFQUJIUQTJ40YPR4GNCQGEJZC0EY1LPODQW4YBRODP')
+
+    #userid = 785079
+    userid = 10000273
     if (params[:id])
-      user = client.user(params[:id])
-    else
-      user = client.user(785079)
+      userid = params[:id]
     end
+    user = client.user(userid)
     #user = client.user(params[:id])
     #if user
     #   @checkins = client.user_checkins(params[:id])
     #end
     #@checkins = user.user_checkins
     render :text => user
+    #render :text => user.user_checkins
   end
 
   def twilio
     account = Twilio::RestAccount.new(ACCOUNT_SID, ACCOUNT_TOKEN)
 
-    resp = account.request("/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/SMS/Messages.json",  'GET')
+    resp = account.request("/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/SMS/Messages",  'GET')
     resp.error! unless resp.kind_of? Net::HTTPSuccess
     #render :text => "code: %s\nbody: %s" % [resp.code, resp.body]
 
-    render :text => resp.body
+    #render :text => resp.body
+
+    body = resp.body
+    body.each do |unit|
+      render :text => unit.sms_messages
+    end
   end
 
 end
